cf83a6a36a9176d3e79dfec82d07f0f9e92b82e4
Graham <gmtelford@gmail.com>
2023-02-02 22:25:38 -0500

    working site and db with logic for auth and no auth pages

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,15 +1,17 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
+from flask_login import login_required, current_user
 from . import db
 
 flaskapp = Blueprint('flaskapp', __name__)
 
 @flaskapp.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 @flaskapp.route('/profile')
+@login_required
 def profile():
-    return 'profile'
+    return render_template('profile.html', user=current_user)
 
 @flaskapp.route('/upload-display')
 def upload():
